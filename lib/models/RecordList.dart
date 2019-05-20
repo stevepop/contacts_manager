b6ccb8002db024b9bf3f58695826b84742f5447d
Steve Popoola <stevepop@gmail.com>
2019-05-20 17:27:31 +0100

    Code format

--- lib/models/RecordList.dart
+++ lib/models/RecordList.dart
@@ -3,16 +3,13 @@ import 'Record.dart';
 class RecordList {
   List<Record> records = List();
 
-    RecordList({
-    this.records
-  });
+  RecordList({this.records});
 
   factory RecordList.fromJson(List<dynamic> parsedJson) {
- 
     List<Record> records = List<Record>();
- 
+
     records = parsedJson.map((i) => Record.fromJson(i)).toList();
- 
+
     return RecordList(
       records: records,
     );
